399526ec56dbbe81944c8b030fc555f9b8a42ef7
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-04-24 01:00:05 +0530

    first react code

---
+++ app.js
@@ -0,0 +1,8 @@
+    const heading = React.createElement("h1", { id: "title" }, "Deepak Maurya");
+    const heading2 = React.createElement("h2", { id: "title" }, "Mishra Ji");
+    const container = React.createElement("div", { id: "container" }, [heading, heading2]);
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+
+    root.render(container);
+
+    
