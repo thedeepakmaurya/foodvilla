a6901286d5c0e2d7a777397ffccb2d0f7a0f9900
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-05-13 17:42:02 +0530

    menu style added

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -9,21 +9,21 @@ const RestaurantMenu = () => {
     const {restaurant, items} = useRestaurant(id); 
 
     return (!restaurant) ? <Shimmer /> : (
-        <div className="menu">
-            <div>
-                <h1>Restaurant id : {id}</h1>
-                <h2>{restaurant?.cards[0]?.card?.card?.text}</h2>
-                <img src={IMG_CDN_URL + restaurant?.cards[2]?.card?.card?.info?.cloudinaryImageId} />
+        <div className="flex justify-center align-middle gap-20 p-10">
+            <div >
+                <h1 className=" mb-2">Restaurant id : {id}</h1>
+                <h2 className="text-xl font-bold mb-1">{restaurant?.cards[0]?.card?.card?.text}</h2>
+                <img className="h-60 w-52 mb-1" src={IMG_CDN_URL + restaurant?.cards[2]?.card?.card?.info?.cloudinaryImageId} />
                 <h3>{restaurant?.cards[2]?.card?.card?.info?.areaName}</h3>
                 <h3>{restaurant?.cards[2]?.card?.card?.info?.city}</h3>
                 <h3>{restaurant?.cards[2]?.card?.card?.info?.avgRating} star</h3>
                 <h3>{restaurant?.cards[2]?.card?.card?.info?.costForTwoMessage}</h3>
             </div>
             <div>
-                <h1>Menu</h1>
+                <h1 className="font-bold text-xl mb-2">Menu</h1>
                 <ul>
                     {Array.isArray(items) && items.map((item) => (
-                        <li key={item.card.info.id}>{item.card.info.name}</li>))}
+                        <li className="list-decimal" key={item.card.info.id}>{item.card.info.name}</li>))}
                 </ul>
             </div>
         </div>
